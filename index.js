a40b489d129c591faa3cf1fdd6feb88d43bece85
touhidcodes <touhidcodes@gmail.com>
2023-07-16 01:57:22 +0600

    payment apis

--- index.js
+++ index.js
@@ -232,6 +232,22 @@ async function run() {
       res.send(result);
     });
 
+    //  Payment APIs
+    // Create payment intent
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
